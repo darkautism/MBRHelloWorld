5fae9f86ea1828419ddc37e91d3c1e07a564852e
kautism <kautism.nung@accelstor.com>
2017-03-16 15:11:54 +0800

    Support GRUB boot

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 boot: boot.c linker.ld
-	@gcc -c -Os -nostdinc -nostdlib -nodefaultlibs -fno-builtin -Wl,--gc-sections -ffunction-sections -fdata-sections -fno-stack-protector -ffreestanding -m32 -Wall -Werror -I. -o boot.o boot.c
+	@gcc ${CFLAGS} -c -Os -nostdinc -nostdlib -nodefaultlibs -fno-builtin -Wl,--gc-sections -fno-stack-protector -ffreestanding -m32 -Wall -Werror -I. -o boot.o boot.c
 	@ld -melf_i386 -static -Tlinker.ld -nostdlib --nmagic -o boot.elf boot.o
 	@objcopy -O binary boot.elf boot.MBR
 	
--- boot.c
+++ boot.c
@@ -1,43 +1,71 @@
 /* barebone from http://dc0d32.blogspot.tw/2010/06/real-mode-in-c-with-gcc-writing.html */
-__asm__(".code16gcc\n");
-__asm__ ("jmpl  $0, $main\n");
+#define SECTOR_SIZE 512
+#ifndef BOOTLOADER_TYPE
+#define BOOTLOADER_TYPE
+/* This is default GRUB boot parameter */
+#define BOOTLOADER_RUNNING_ADDRESS 0x8200
+#define BOOTLOADER_LOAD_ADDRESS 0x8000
+#endif
+
+#ifndef BOOTLOADER_START_SECTOR
+#define BOOTLOADER_START_SECTOR 1
+#endif
+
+#ifndef BOOTLOADER_SIZE
+#define BOOTLOADER_SIZE 131368
+#endif
+#define BOOTLOADER_SIZE_SECTOR (BOOTLOADER_SIZE/SECTOR_SIZE + ((BOOTLOADER_SIZE%SECTOR_SIZE)?1:0))
+
+
+asm(".code16gcc\n");
+asm ("jmp main\n") ;
+
 #define __NOINLINE  __attribute__((noinline))
 #define __REGPARM   __attribute__ ((regparm(3)))
 #define __NORETURN  __attribute__((noreturn))
 
-struct buffer_packet
+struct lba_reader_packet
 {
-	short buffer_packet_size;         /* struct's size ( 0x10 or  0x18 )*/
-	short sectors;                    /* read size of sectors */
-	char * buffer;                     /* buffer address */
-	long long start_sectors;
-	long long *l_buffer;              /* 64 bit buffer address */
+	unsigned short struct_size;				/* struct's size ( 0x10 or  0x18 )*/
+	unsigned short sectors;							/* read size of sectors */
+	unsigned int bootloader_load_address;		/* buffer address */
+	unsigned long long start_sector;
 } ; /* struct from http://www.mouseos.com/win7/int_0x13.html */
 
-struct buffer_packet buffer = {
-	.buffer_packet_size=0x10,
-	.sectors=5,
-	.buffer=(char *)0x8000,
-	.start_sectors=1
-	};
- 
-/* BIOS interrupts must be done with inline assembly */
+struct lba_reader_packet lbapkt = {
+		.struct_size = sizeof(struct lba_reader_packet),
+		.sectors = BOOTLOADER_SIZE_SECTOR,						/* bootloader size of sector */
+		.bootloader_load_address = BOOTLOADER_LOAD_ADDRESS,		/* bootloader memory loaded address */
+		.start_sector = BOOTLOADER_START_SECTOR				/* bootloader start sector */
+		};
+		
 void __NOINLINE __REGPARM print(const char *s) {
 	while(*s)
-		__asm__ __volatile__ ("int  $0x10" : : "a"(0x0E00 | *s++), "b"(7));
+		asm volatile ("int  $0x10" : : "a"(0x0E00 | *s++), "b"(7));
 }
 
-void __NOINLINE __REGPARM load_sector(unsigned char boot_drive) {
-	__asm__ __volatile__ ("int $0x13" : : "a"(0x4200), "d"(boot_drive), "S"(&buffer):"cc");
+int __NOINLINE __REGPARM lba_load_sector(unsigned char boot_drive) {
+	asm volatile goto ("int $0x13\njc %l[failed]"
+		: 
+		: "a"(0x4200), "d"(boot_drive), "S"(&lbapkt)
+		:"cc"
+		: failed);
+return 0;
+failed:
+return 1;
 }
 
 /* and for everything else you can use C! Be it traversing the filesystem, or verifying the kernel image etc.*/
 void __NORETURN main(){
  	unsigned char   bios_drive = 0;
-	__asm__ __volatile__("movb  %%dl, %0" : "=r"(bios_drive));      /* the BIOS drive number of the device we booted from is passed in dl register */
+	asm volatile("movb  %%dl, %0" : "=r"(bios_drive));      /* the BIOS drive number of the device we booted from is passed in dl register */
 	print("Hello World!\r\n");
-	load_sector(bios_drive);
-	//__asm__ ("jmpl  $0, $0x8200");
-	while(1);
+	if( lba_load_sector(bios_drive)) {
+		print("Load bootloader Failed");
+		while(1);
+	}
+	print("Boot bootloader...\r\n");
+	asm volatile ("" : : "d"(bios_drive));
+	goto *((void *) BOOTLOADER_RUNNING_ADDRESS);
 }
 
